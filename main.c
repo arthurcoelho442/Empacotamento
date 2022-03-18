d479f93235cf9f1b198557849fd9468e6aa996d7
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2022-03-18 14:33:42 -0300

    Base

--- main.c
+++ main.c
@@ -3,20 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 
-typedef struct lista {
-    ordenacao *ini, *fim;
-} Lista;
-
-void insereNaLista(Lista *l, ordenacao *e);
-void excluiLista(Lista *l);
 int comp_suf_array(const void *pa, const void * pb);
 int main(int argc, char** argv) {
-    Lista *lista = (Lista*) malloc(sizeof (lista));         //Lista com todas as ordenações executadas
+    //Lista *lista = (Lista*) malloc(sizeof (lista));         //Lista com todas as ordenações executadas
     FILE* entrada;                                          //Arquivo de entrada para o sistema
     FILE* saida;                                            //Arquivo de saida
 
     //Abre o arquivo de Entrada e o de Saida
-    entrada = fopen(argv[3], "r+");
+    entrada = fopen(argv[1], "r+");
     saida = fopen("saida.txt", "a+");
 
     //Verifica se o arquivo de Entrada é existente
@@ -38,36 +32,19 @@ int main(int argc, char** argv) {
         fscanf(entrada, "%ld", &dados[i]);
     ///////////////////////////////////////////
 
+    for(long int i = 0; i < qtd; i++)
+        printf("%ld\n", dados[i]);
     qsort((void*)dados, qtd, sizeof(int*), comp_suf_array);
+    
+    printf("\n\n");
+    
+    for(long int i = 0; i < qtd; i++)
+        printf("%ld\n", dados[i]);
 
     fclose(entrada);
     fclose(saida);
     
     return (EXIT_SUCCESS);
-}
-
-//Insere os dados de uma ordenação na lista
-void insereNaLista(Lista *l, ordenacao *e) {
-    //Entra se a lista esta vazia e adiciona os dados
-    if (l->ini == NULL) {
-        l->ini = e;
-        l->fim = e;
-    } else {//Entra se a lista já possui elementos e adiciona na ultima posição
-        l->fim->prox = e;
-        l->fim = e;
-    }
-}
-
-//Libera o espaço da alocação dinamica 
-void excluiLista(Lista *l) {
-    //Anda pela fila leberando algoritmo por algoritmo
-    for (ordenacao *p = l->ini; p != NULL;){
-        ordenacao* t = p->prox;
-        libera(p);
-        p = t;
-    }
-    
-    free(l); //Libera a lista
 }
 
 int comp_suf_array(const void *pa, const void * pb){
