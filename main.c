455d317b9fbb90750000afba270fd8c78ef72e42
arthurcoelho442 <arthurcoelho442@gmail.com>
2022-03-18 14:00:05 -0300

    inicio

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct lista {
+    ordenacao *ini, *fim;
+} Lista;
+
+void insereNaLista(Lista *l, ordenacao *e);
+void excluiLista(Lista *l);
+int comp_suf_array(const void *pa, const void * pb);
 int main(int argc, char** argv) {
     Lista *lista = (Lista*) malloc(sizeof (lista));         //Lista com todas as ordenações executadas
     FILE* entrada;                                          //Arquivo de entrada para o sistema
@@ -31,8 +38,39 @@ int main(int argc, char** argv) {
         fscanf(entrada, "%ld", &dados[i]);
     ///////////////////////////////////////////
 
+    qsort((void*)dados, qtd, sizeof(int*), comp_suf_array);
+
     fclose(entrada);
     fclose(saida);
     
     return (EXIT_SUCCESS);
+}
+
+//Insere os dados de uma ordenação na lista
+void insereNaLista(Lista *l, ordenacao *e) {
+    //Entra se a lista esta vazia e adiciona os dados
+    if (l->ini == NULL) {
+        l->ini = e;
+        l->fim = e;
+    } else {//Entra se a lista já possui elementos e adiciona na ultima posição
+        l->fim->prox = e;
+        l->fim = e;
+    }
+}
+
+//Libera o espaço da alocação dinamica 
+void excluiLista(Lista *l) {
+    //Anda pela fila leberando algoritmo por algoritmo
+    for (ordenacao *p = l->ini; p != NULL;){
+        ordenacao* t = p->prox;
+        libera(p);
+        p = t;
+    }
+    
+    free(l); //Libera a lista
+}
+
+int comp_suf_array(const void *pa, const void * pb){
+    if((*(int*)pa) < (*(int*)pb)) return 1;
+    else return 0;
 }
