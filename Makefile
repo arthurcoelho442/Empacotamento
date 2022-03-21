649eee7c20e886d4de2f6f86a70c76767f0c3f4c
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2022-03-21 14:23:59 -0300

    V7

--- Makefile
+++ Makefile
@@ -1,35 +1,48 @@
 #Antes de executar o progama entre na pasta onde 
 #o programa esta localizado.
+echo = echo
 
-
+#para executar compile digite no terminal "make compile"
 compile:
 	gcc -o trab3 *.c	
-	
+
+#para executar run digite no terminal "make run arg=diretoriodo_arquivo"	
 run:
 	./trab3 $(arg)
 	
-compile-run:
+#para executar compile-run digite no terminal "make compile-run arg=diretoriodo_arquivo"
+compile-run: 
 	make compile
 	make run
-	
+
+#para executar compile-run-all digite no terminal "make compile-run-all"
 compile-run-all:
-	gcc -o trab3 print-all/cabecario.c
-	./trab3
-	
+	echo "-------------------------------------------------------------------------------" > ED2_Trab3_results.txt
+	echo "                           Number of disks" >> ED2_Trab3_results.txt
+	echo "\t\tWorst\tBest\tWorst-Fit\tBest-Fit\tRunning time" >> ED2_Trab3_results.txt
+	echo "\tN\tFit\tFit\tDecreasing\tDecreasing\t(seconds)" >> ED2_Trab3_results.txt
+	echo "-------------------------------------------------------------------------------" >> ED2_Trab3_results.txt
 	make compile
-	
 	make run arg=in/5.txt
 	make run arg=in/20.txt
 	make run arg=in/100.txt
 	make run arg=in/1000.txt
 	make run arg=in/10000.txt
 	make run arg=in/100000.txt
+	echo "-------------------------------------------------------------------------------" >> ED2_Trab3_results.txt
 	
-	gcc -o trab3 print-all/challenge.c disco.c
+	echo "(Challenge)" >> ED2_Trab3_results.txt
+	gcc -o trab3 challenge/challenge.c disco.c
 	make run arg=in/1000000.txt
-	
+	echo "-------------------------------------------------------------------------------" >> ED2_Trab3_results.txt
+#para executar valgrind digite no terminal "make valgrind arg=diretoriodo_arquivo"
 valgrind:
 	make compile
 	valgrind ./trab3 $(arg)
-
+#para executar clear digite no terminal "make clear"
+clean:
+	#rm *.o
+	rm trab3
+	rm ED2_Trab3_results.txt
+	clear
 #Finalizado
--- challenge/challenge.c
+++ challenge/challenge.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "disco.h"
+#include "../disco.h"
 
-int comp_suf_array(const void *pa, const void * pb);
 int main(int argc, char** argv) {
     FILE* entrada;                                          //Arquivo de entrada para o sistema
     FILE* saida;                                          //Arquivo de saida para o sistema
@@ -19,7 +18,7 @@ int main(int argc, char** argv) {
         return 0;
     }
     ///////////////////////////////////////
-
+    
     //Tratamento do dados do arquivo //////////
     int qtd;                                                //quantidade de itens
     fscanf(entrada, "%d", &qtd);
@@ -30,53 +29,33 @@ int main(int argc, char** argv) {
     ///////////////////////////////////////////
     fprintf(saida, "%7d", qtd);
     
-    long int size = 0;
+    /*long int size = 0;
     for(long int i = 0; i < qtd; i++)
         size +=dados[i];
     
-    Lista *l,*l2,*l3,*l4;
+    Lista *l1;
 
     clock_t init = clock();
         
     printf("\nTotal size(GB): %0.2lf\n", size/TAM_SIZE);
     printf("\nNumber of disks:\n");
     
-    l = worstFit(l,dados, qtd);
-    l2 = bestFit(l2, dados, qtd);    
-    
-    printf("Wors-fit: %d\n", l->tam);    
-    printf("Best-fit: %d\n", l2->tam);
-    fprintf(saida, "\t%d", l->tam);
-    fprintf(saida, "\t%d", l2->tam);
-    
-    qsort((void*)dados, qtd, sizeof(int*), comp_suf_array);
-    
-    l3 = worstFit(l3,dados, qtd);
-    l4 = bestFit(l4, dados, qtd);    
-
-    printf("Wors-fit decreasing: %d\n", l3->tam);
-    printf("Best-fit decreasing: %d\n", l4->tam);
-    fprintf(saida, "\t%d", l3->tam);
-    fprintf(saida, "\t\t%d", l4->tam);
+    l1 = bestFit(l1, dados, qtd);    
+     
+    printf("Best-fit: %d\n", l1->tam);
+    fprintf(saida, "\t\t%d", l1->tam);
     
     clock_t fim = clock();                              
     double time = (double)(fim - init)/CLOCKS_PER_SEC;
 
     printf("\ntempo: %lf\n", time);
-    fprintf(saida, "\t\t%lf\n", time);
+    fprintf(saida, "\t\t\t\t\t%lf\n", time);
     
-    excluiLista(l);
-    excluiLista(l2);
-    excluiLista(l3);
-    excluiLista(l4);
-
-    fclose(entrada);
+    excluiLista(l1);*/
+    fprintf(saida, "\n");
+    
     free(dados);
-    
+    fclose(saida);    
+    fclose(entrada);
     return (EXIT_SUCCESS);
-}
-
-int comp_suf_array(const void *pa, const void * pb){
-    if((*(int*)pa) < (*(int*)pb)) return 1;
-    else return 0;
 }
