43b8afae60c4c1a3f4a79b79e8e3ac0da46c60c8
arthurcoelho442 <arthurcoelho442@gmail.com>
2022-03-22 21:10:41 -0300

    Finalizado

--- disco.c
+++ disco.c
@@ -1,45 +1,50 @@
 #include "disco.h"
 
 
-Lista* inicLista();
-Lista* insereNaLista(Lista *l, disco *e);
-Lista* ordenaLista(Lista* l);
-disco* pegaIdeal(Lista* l, int dados);
+Lista* inicLista();                         //Inicia a lista de disco
+Lista* insereNaLista(Lista *l, disco *e);   //Insere disco na lista de discos
+Lista* ordenaLista(Lista* l);               //Organiza a lista de disco de foram decresente
+disco* pegaIdeal(Lista* l, int dados);      //Pega o Menor disco que caiba o arquivo
 
 Lista* worstFit(Lista* l, long int* dados, int qtd){
 
-    l = inicLista();
+    l = inicLista(); //Inicia a lista
     
+    //Pecorre a lista de arquivos
     for(int i=0; i<qtd; i++){
+        //Entra no if caso a lista esteja vazia
         if(l->tam == 0){
-            disco* d = criaDisco(qtd);
-            l = insereNaLista(l, d);
+            disco* d = criaDisco(qtd);  //Cria o disco
+            l = insereNaLista(l, d);    //Insere o disco na lista
         }
         disco* aux = l->ini;
+        //Verifica se o arquivo cabe no disco com maior espaço restante
         if(aux->tam_rest >= dados[i]){
-            aux->tam_rest -= dados[i];
-        }else {
+            aux->tam_rest -= dados[i]; //incrementa o tamanho do arquivo no disco
+        }else { //Cria o disco e insere na lista e incrementa o tamanho do arquivo no disco
             disco* d = criaDisco(qtd);
             l = insereNaLista(l, d);
             l->ini->tam_rest -= dados[i];
         }
         l = ordenaLista(l);
     }
-    return l;
+    return l;//Retorna a lista de discos
 }
 Lista* bestFit(Lista* l, long int* dados, int qtd){
 
-    l = inicLista();
+    l = inicLista(); //Inicia a lista
     
+    //Percore a lista de arquivos
     for(int i=0; i<qtd; i++){
+        //Entra no if caso a lista esteja vazia
         if(l->ini == NULL){
             disco* d = criaDisco(qtd);
             l = insereNaLista(l, d);
         }
-        disco* ideal = pegaIdeal(l, dados[i]);
-        if(ideal != NULL){
-            ideal->tam_rest-=dados[i];
-            if(ideal->tam_rest == 0){
+        disco* ideal = pegaIdeal(l, dados[i]); //Pega o disco ideal para se armazenar o arquivo
+        if(ideal != NULL){//Entra caso haja disco ideal
+            ideal->tam_rest-=dados[i];  //Coloca o arquivo no disco
+            if(ideal->tam_rest == 0){   //Verifica se o disco esta vazio e o remove
                 disco* ant = ideal->ant;
                 disco* prox = ideal->prox;
                 
@@ -56,19 +61,19 @@ Lista* bestFit(Lista* l, long int* dados, int qtd){
                 }
                 free(ideal);
             }
-        }else {
+        }else {                         //Entra caso não caiba o disco e cria um novo disco
                 disco* d = criaDisco(qtd);
                 d->tam_rest -= dados[i];
                 l = insereNaLista(l, d);
         }
     }
-    return l;
+    return l;//Retorna a lista de discos
 }
 
 disco* criaDisco(){
-    disco* d = (disco*) malloc(sizeof(disco));
-    d->tam_rest = TAM_SIZE;
-    d->ant = NULL;
+    disco* d = (disco*) malloc(sizeof(disco));  //Aloca o disco dinamicamente
+    d->tam_rest = TAM_SIZE;                     //Inicia o disco com o tamanho de 1G
+    d->ant = NULL;                              
     d->prox = NULL;
     return d;
 }
@@ -108,17 +113,17 @@ Lista* ordenaLista(Lista* l){
     
     //Ordenação Decresente
     for(; prox!=NULL; ant = prox, prox = atual->prox){
-        if(atual->tam_rest == 0){
-            if (prox != NULL){
+        if(atual->tam_rest == 0){   //Verifica se o disco esta vazio 
+            if (prox != NULL){      //Entra caso hajá mais discos
                 l->ini = prox;
                 prox->ant = NULL;
-            }else{
+            }else{                  //Entra se ele é o ultimo disco da lista
                 l->ini = NULL;
                 l->fim = NULL;
             }
             free(atual);
-            break;
-        }else if(atual->tam_rest < prox->tam_rest){
+            break;                  
+        }else if(atual->tam_rest < prox->tam_rest){ //Entra caso o proximo disco seja maior e troca os discos
            if(l->ini == atual)
                l->ini = prox;
            atual->prox = prox->prox;
@@ -129,21 +134,22 @@ Lista* ordenaLista(Lista* l){
            
            if(ant != NULL)
                ant->prox = prox;
-        }else
+        }else //Lista esta ordenada sai do loop
            break;
     }
     
-    return l;
+    return l; //Retorna a lista de discos
 }
 disco* pegaIdeal(Lista* l, int dado){
-    int ideal = TAM_SIZE + 1;
+    int ideal = TAM_SIZE + 1; //inicia o tamanho ideal como sendo 1G + 1
     disco* dIdeal = NULL;
+    //Percorre a lista de discos
     for(disco* aux = l->ini; aux != NULL; aux = aux->prox)
-        if(aux->tam_rest >= dado && aux->tam_rest < ideal){
+        if(aux->tam_rest >= dado && aux->tam_rest < ideal){// Entra caso o tamanho do disco caiba o dado e se ele for menor que o tamanho encontrado até o momento
             ideal = aux->tam_rest;
             dIdeal = aux;
         }
-    return dIdeal;
+    return dIdeal; //Retorna o tamanho do disco
 }
 
 //Libera o espaço da alocação dinamica 
